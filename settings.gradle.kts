94c7dc99c6697260b8e4af87a4a7dd64b46fa97e
Adrien Poupard <1469248+apoupard@users.noreply.github.com>
2024-03-20 09:42:43 +0100

    feat(actions): add support for enabling gradle build scan publish in JVM action feat(actions): update setup-gradle-github-pkg action to feat/docs branch feat(workflows): update dev workflow to use feat/docs branch for JVM workflow feat(workflows): update make-jvm-workflow to use feat/docs branch for actions feat(workflows): update make-nodejs-workflow to use feat/docs branch for actions feat(workflows): update publish-storybook-workflow to use feat/docs branch for actions The changes were made to enhance the functionality of the actions and workflows by adding support for enabling gradle build scan publish and updating the workflow references to use the feat/docs branch for improved documentation.
    feat(sec-workflow.yml): add support for enabling gradle build scan publish feature
    feat(settings.gradle.kts): remove redundant gradleEnterprise block and simplify build configuration
    The changes in sec-workflow.yml introduce a new boolean input `with-gradle-build-scan-publish` to enable or disable the gradle build scan publish feature. This provides more flexibility in controlling the behavior of the workflow. In settings.gradle.kts, the gradleEnterprise block is removed as it is redundant and the build configuration is simplified for better readability and maintenance.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,7 @@
 rootProject.name = "fixers-gradle"
 
-pluginManagement {
-	repositories {
-		gradlePluginPortal()
-	}
-}
-
-plugins {
-	id("com.gradle.enterprise") version "3.16.2"
-}
-
 includeBuild("build-composite")
 
 include("config")
 include("dependencies")
 include("plugin")
-
-gradleEnterprise {
-	if (System.getenv("CI") != null) {
-		buildScan {
-			publishAlways()
-			termsOfServiceUrl = "https://gradle.com/terms-of-service"
-			termsOfServiceAgree = "yes"
-		}
-	}
-}
