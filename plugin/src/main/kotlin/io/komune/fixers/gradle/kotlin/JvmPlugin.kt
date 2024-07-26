6673a756df40d514bd1508c1dbc6068cd593868b
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-07-26 11:36:39 +0200

    feat(plugin): add logging statements to provide more information during plugin execution refactor(plugin): refactor configureJvmCompilation function to be an extension function of Project class for better readability and organization The logging statements have been added to provide more visibility into the plugin execution process, making it easier to track the flow and identify any potential issues. The configureJvmCompilation function has been refactored into an extension function of the Project class to improve readability and maintainability of the codebase.

--- plugin/src/main/kotlin/io/komune/fixers/gradle/kotlin/JvmPlugin.kt
+++ plugin/src/main/kotlin/io/komune/fixers/gradle/kotlin/JvmPlugin.kt
@@ -9,56 +9,68 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.kotlin
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 @Suppress("UnstableApiUsage")
 class JvmPlugin : Plugin<Project> {
 
 	override fun apply(target: Project) {
-		configureJvmCompilation(target)
+		target.logger.info("Applying JvmPlugin to project: ${target.name}")
+		target.apply(plugin = "java")
+		target.apply(plugin = "org.jetbrains.kotlin.jvm")
+		target.configureJvmCompilation()
 		target.setupJarInfo()
 	}
 
-	private fun configureJvmCompilation(target: Project) {
-		target.apply(plugin = "java")
-		target.apply(plugin = "org.jetbrains.kotlin.jvm")
-		target.plugins.apply(ConfigPlugin::class.java)
-		val fixersConfig = target.rootProject.extensions.fixers
+	private fun Project.configureJvmCompilation() {
+		logger.info("Configuring JVM compilation for project: ${name}")
+		plugins.apply(ConfigPlugin::class.java)
+		val fixersConfig = rootProject.extensions.fixers
 		val jdkVersion = fixersConfig?.jdk?.version ?: Jdk.VERSION_DEFAULT
 
-		target.tasks.withType<KotlinCompile>().configureEach {
-			println("Configuring $name in project ${project.name}...")
+		logger.info("Using JDK version: $jdkVersion")
+
+		kotlinExtension.jvmToolchain(jdkVersion)
+		tasks.withType<KotlinCompile>().configureEach {
+			logger.info("Configuring Kotlin compile task: $name in project ${project.name}")
 			kotlinOptions {
-				freeCompilerArgs = listOf("-Xjsr305=strict",  "-opt-in=kotlin.js.ExperimentalJsExport")
+				freeCompilerArgs = listOf("-Xjsr305=strict")
 				jvmTarget = jdkVersion.toString()
 				languageVersion = FixersPluginVersions.kotlin.substringBeforeLast(".")
 			}
 		}
 
-		target.plugins.withType(JavaPlugin::class.java).whenPluginAdded {
-			target.extensions.configure(JavaPluginExtension::class.java) {
+		plugins.withType(JavaPlugin::class.java).whenPluginAdded {
+			tasks.withType(JavaCompile::class) {
+				logger.info("Configuring Java compile task: $name in project ${project.name}")
+				options.release.set(jdkVersion)
+			}
+			extensions.configure(JavaPluginExtension::class.java) {
 				toolchain.languageVersion.set(JavaLanguageVersion.of(jdkVersion))
 			}
 		}
 
-
-		target.dependencies {
+		dependencies {
+			logger.info("Configuring dependencies for project: $name")
 			add("implementation", kotlin("reflect"))
-			FixersDependencies.Jvm.Kotlin.coroutines{
+			FixersDependencies.Jvm.Kotlin.coroutines {
 				add("implementation", it)
 			}
-			FixersDependencies.Jvm.Test.junit{
+			FixersDependencies.Jvm.Test.junit {
 				add("testImplementation", it)
 			}
 		}
 
-		target.tasks.withType<Test> {
+		tasks.withType<Test> {
+			logger.info("Configuring test task: $name in project ${project.name}")
 			useJUnitPlatform()
 		}
 	}
