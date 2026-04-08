bed0e64152d692e127236b4559bbe619d4680d8f
Adrien Poupard <adrien@komune.io>
2026-04-08 11:24:09 +0200

    Fix cross-platform sed -i in github.mk
    Use sed -i.bak instead of macOS-only sed -i '' so action ref
    pinning works on both macOS and Linux CI runners.

--- infra/make/github.mk
+++ infra/make/github.mk
@@ -5,5 +5,7 @@ ifndef VERSION
 	$(error Usage: make tag-github-actions VERSION=0.31.0)
 endif
 	@echo "Pinning action refs to @$(VERSION)"
-	find .github/workflows -name '*.yml' -exec sed -i '' '/komune-io\/fixers-gradle/s|@[^"'"'"' ]*|@$(VERSION)|g' {} +
-	find .github/actions -name 'action.yml' -exec sed -i '' '/komune-io\/fixers-gradle/s|@[^"'"'"' ]*|@$(VERSION)|g' {} +
+	find .github/workflows -name '*.yml' -exec sed -i.bak '/komune-io\/fixers-gradle/s|@[^"'"'"' ]*|@$(VERSION)|g' {} +
+	find .github/workflows -name '*.yml.bak' -delete
+	find .github/actions -name 'action.yml' -exec sed -i.bak '/komune-io\/fixers-gradle/s|@[^"'"'"' ]*|@$(VERSION)|g' {} +
+	find .github/actions -name '*.bak' -delete
